400f6f29e327b8b762c86d77a959759bd7005f0c
Adam-west99 <aw.west48@gmail.com>
2025-05-02 13:43:57 +0100

    Update weapons.cs

--- weapons.cs
+++ weapons.cs
@@ -1,1 +1,30 @@
+using System;
+using DungeonExplorer.monsters;
+
+namespace DungeonExplorer
+{
+    public class Weapon : Item
+    {
+        public int Damage { get; set; }
+
+        public Weapon(string name, int damage)
+        {
+            Name = name;
+            Damage = damage;
+        }
+
+        public override void Use(Creature target)
+        {
+            if (target is IDamageable damageable)
+            {
+                Console.WriteLine($"Using {Name} to deal {Damage} damage to {target.Name}.");
+                damageable.TakeDamage(Damage);
+            }
+            else
+            {
+                Console.WriteLine($"{target.Name} cannot be attacked with a weapon.");
+            }
+        }
+    }
+}
 
