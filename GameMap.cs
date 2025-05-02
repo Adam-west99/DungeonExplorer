a7ad4c097db5cd302abe1ced9e3fe01a24c5e1e5
Adam-west99 <aw.west48@gmail.com>
2025-05-02 13:37:31 +0100

    Update GameMap.cs

--- GameMap.cs
+++ GameMap.cs
@@ -1,1 +1,10 @@
+using system 
+namespace DungeonExplorer
+{  public class GameMap
+  {
+      private Dictionary<string, Room> rooms = new Dictionary<string, Room>();
 
+      public void AddRoom(string id, Room room) => rooms[id] = room;
+      public Room GetRoom(string id) => rooms.TryGetValue(id, out var room) ? room : null;
+  }
+}
