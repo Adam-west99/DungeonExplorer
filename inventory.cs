1b00c3fbd9354ad63c3489522dcdbe295ed53c4b
Adam-west99 <aw.west48@gmail.com>
2025-05-02 13:36:06 +0100

    Update inventory.cs

--- inventory.cs
+++ inventory.cs
@@ -1,1 +1,13 @@
+using System;
 
+namespace DungeonExplorer
+{
+    public class Inventory
+  {
+      private List<Item> items = new List<Item>();
+
+      public void AddItem(Item item) => items.Add(item);
+      public void RemoveItem(Item item) => items.Remove(item);
+      public IEnumerable<T> GetItemsOfType<T>() where T : Item => items.OfType<T>();
+  }
+}
