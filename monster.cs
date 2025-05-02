c0b72cfb6d7d6e68be2f82e7403a2d1ebfd20ccf
Adam-west99 <aw.west48@gmail.com>
2025-05-02 13:34:12 +0100

    Update monster.cs

--- monster.cs
+++ monster.cs
@@ -1,15 +1,36 @@
+using System;
 
-  public class Monster : Creature, IDamageable
-  {
-      public int AttackPower { get; set; }
+namespace DungeonExplorer.Models
+{
+    public class Monster : Creature, IDamageable
+    {
+        public int AttackPower { get; set; }
+        public bool IsAlive => Health > 0;
 
-      public override void Attack(Creature target)
-      {
-          // Implement attack logic
-      }
+        public override void Attack(Creature target)
+        {
+            if (target is IDamageable damageableTarget)
+            {
+                Console.WriteLine($"{Name} attacks {target.Name} for {AttackPower} damage!");
+                damageableTarget.TakeDamage(AttackPower);
+            }
+            else
+            {
+                Console.WriteLine($"{target.Name} cannot be attacked.");
+            }
+        }
 
-      public void TakeDamage(int amount)
-      {
-          // Implement damage logic
-      }
-  }
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            Console.WriteLine($"{Name} takes {amount} damage!");
+
+            if (Health <= 0)
+            {
+                Health = 0;
+                Console.WriteLine($"{Name} has been defeated!");
+            }
+        }
+    }
+}
+
