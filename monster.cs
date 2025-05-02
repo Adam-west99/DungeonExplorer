d2e9eb7c4937d9ed85cd56d92ba3f9e570516052
Adam-west99 <aw.west48@gmail.com>
2025-05-02 13:32:16 +0100

    Update monster.cs

--- monster.cs
+++ monster.cs
@@ -1,1 +1,15 @@
 
+  public class Monster : Creature, IDamageable
+  {
+      public int AttackPower { get; set; }
+
+      public override void Attack(Creature target)
+      {
+          // Implement attack logic
+      }
+
+      public void TakeDamage(int amount)
+      {
+          // Implement damage logic
+      }
+  }
